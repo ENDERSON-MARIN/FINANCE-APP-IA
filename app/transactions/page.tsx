dc7cf05e5591b14362b6f3e551695b7d71ff0411
ENDERSON-MARIN <marinenderson1@gmail.com>
2025-09-18 14:18:52 -0300

    feat: implement transactions table with react-table

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,28 @@
-const TransactionsPage = () => {
-  return <h1>Transactions Page</h1>;
+// import { db } from "../_lib/prisma";
+
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { DataTable } from "../_components/ui/data-table";
+import { TransactionColumns } from "./_columns";
+import { db } from "../_lib/prisma";
+
+const TransactionsPage = async () => {
+  //accesar a las transacciones de la base de datos
+  const transactions = await db.transaction.findMany();
+
+  return (
+    <div className="space-y-6 p-6">
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Transactions</h1>
+        <Button className="rounded-full">
+          Add Transaction
+          <ArrowDownUpIcon className="ml-1 inline-block" />
+        </Button>
+      </div>
+
+      <DataTable columns={TransactionColumns} data={transactions} />
+    </div>
+  );
 };
 
 export default TransactionsPage;
