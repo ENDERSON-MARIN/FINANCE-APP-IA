d477c0b9c032d422c5adfb61cde831d61c7cc645
ENDERSON-MARIN <marinenderson1@gmail.com>
2025-09-23 21:54:45 -0300

    feat: add Navbar component

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,14 @@
+import Navbar from "../_components/navbar";
+
 const SubscriptionPage = () => {
-  return <h1>Subscription Page</h1>;
+  return (
+    <>
+      <Navbar />
+      <div className="space-y-6 p-6">
+        <h1 className="text-2xl font-bold">Assinatura</h1>
+      </div>
+    </>
+  );
 };
 
 export default SubscriptionPage;
