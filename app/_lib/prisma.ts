c9fa71e5ab0ea6977dc3dd827b63d49ad4010b33
ENDERSON-MARIN <marinenderson1@gmail.com>
2026-07-09 18:00:11 -0300

    chore: migra npm para pnpm e actualiza dependências para Next 16 / React 19 / Prisma 6

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var cachedPrisma: PrismaClient; // eslint-disable-line no-var
+  var cachedPrisma: PrismaClient;
 }
 
 let prisma: PrismaClient;
